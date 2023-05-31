23d1da8ce0cfb746460e49049b329a0fdd0fa387
gryan-p <110953018+gryan-p@users.noreply.github.com>
2023-05-31 17:24:55 +0800

    Add files via upload

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    //baseUrl : 'https://login.test.mymunters.com/',
+    baseUrl: 'https://login.mymunters.com/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  env: {
+    timeout: '30000'
+  }
+  },
+});
